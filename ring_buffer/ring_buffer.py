b9d6817727ba526f87c29c042666edc28c423a4b
Jose Montero <josemariodev@gmail.com>
2020-02-28 11:09:19 -0600

    Refactor ring_buffer, tests passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,35 +8,45 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        if self.storage.length < self.capacity:
-            self.storage.add_to_head(item)
-            self.current = self.storage.tail
-        elif self.current is self.storage.tail:
-            self.storage.remove_from_tail()
+        # if ring isn't full add to ring set to tail
+        if self.capacity > self.storage.length:
             self.storage.add_to_tail(item)
-            self.current = self.current.prev
-        elif self.current is self.storage.head:
+            # also set to head
+            self.current = self.storage.head
+        # if ring is full delete head
+        elif self.capacity == self.storage.length:
+            # head is oldest item in ring
+            oldest_item = self.storage.head
+            # delete head
             self.storage.remove_from_head()
-            self.storage.add_to_head(item)
-            self.current = self.storage.tail
-        else:
-            self.current.insert_after(item)
-            self.current.delete()
-            self.current = self.current.prev
+            # add item to tail
+            self.storage.add_to_tail(item)
+            # if only 1 item in ring, set to head and tail
+            if oldest_item == self.current:
+                self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
+        # add first item
+        first_item = self.current
+        list_buffer_contents.append(first_item.value)
 
-        current = self.storage.head
-        while current != None:
-            list_buffer_contents.insert(0, current.value)
-            if current.next == None:
-                break
+        # check to see if there is next item
+        if first_item.next:
+            next_item = first_item.next
+        else:
+            next_item = self.storage.head
+
+        # loop through dll, appending items till done
+        while next_item is not first_item:
+            list_buffer_contents.append(next_item.value)
+            if next_item.next:
+                next_item = next_item.next
             else:
-                current = current.next
-
+                next_item = self.storage.head
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
